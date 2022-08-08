a83a9769dd148cf151f0f46256278f772fb06596
Nima Payandeh <nimapayandeh11@gmail.com>
2022-08-08 22:26:18 +0430

    add theme to app

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kbackgroundColor = Color(0xFF0a0f29);
+const kblueColor = Color(0xFF2666cf);
+const ktranparentColor = Colors.white54;
---
+++ lib/input_page.dart
@@ -0,0 +1,18 @@
+import 'package:bmi_calculator/constants.dart';
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kbackgroundColor,
+    );
+  }
+}
